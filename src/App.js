77bdd494c30a8223e6b2b65161620280c51cb546
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-05-23 16:33:58 +0500

    form added and outputting value into console

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React from 'react';
 
 
 function App() {
+  let Name1 = '';
+  let Age1 = '';
+  const userNameHandler = (event) => {
+    console.log(event.target.value);
+
+    Name1 = event.target.value;
+  };
+  const userAgeHandler = (event) => {
+    console.log(event.target.value);
+
+    Age1 = event.target.value;
+
+  };
+
+  const submitClickHandler = (event) => {
+    event.preventDefault();
+    console.log('User Name ' + Name1, 'Age' + Age1);
+
+
+  };
+
   return (
     <div>
+      <form onSubmit={submitClickHandler}>
+        <label>User name </label>
+        <input type="text" onChange={userNameHandler} />
+        <label>Age </label>
+        <input type="number" onChange={userAgeHandler} />
 
+        <button type='submit'> Submit</button>
+      </form>
 
     </div>
   );
-}
+};
 
 export default App;
